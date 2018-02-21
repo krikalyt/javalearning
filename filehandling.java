443444905006057d64e52ea6069c34fd9037c3d1
krikalyt <34827447+krikalyt@users.noreply.github.com>
2018-02-21 17:56:15 +0530

    Add files via upload

---
+++ filehandling.java
@@ -0,0 +1,59 @@
+package javafilehandling;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class filehandling 
+{
+   public static void main(String[] args) throws Exception 
+   {
+       //checking file exit or not
+//        File x = new File("C:\\Users\\KRISHNA\\Desktop\text.txt");
+//        if(x.exists())
+//        {
+//            System.out.println("file found");
+//        }
+//        else
+//            System.out.println("file not found");
+       
+       
+       //file generating
+       
+//       try {
+//    Formatter f = new Formatter("C:\\\\Users\\\\KRISHNA\\\\Desktop\\testing.txt");
+//  } catch (Exception e) {
+//      System.out.println("Error");
+//  }
+       
+       //file reading
+       
+//       try 
+//       {
+//               File x = new File("C:\\\\\\\\Users\\\\\\\\KRISHNA\\\\\\\\Desktop\\\\testing.txt");
+//               Scanner sc = new Scanner(x); 
+//               while(sc.hasNext())
+//               {
+//                   System.out.println(sc.next());
+//               }
+//       }
+//       catch (FileNotFoundException e) 
+//       {
+//              System.out.println("file handling");
+//       }
+       
+       //data input to file
+       
+//         try {
+//    Formatter f = new Formatter("C:\\\\Users\\\\KRISHNA\\\\Desktop\\testing.txt");
+//    f.format("%s %s %s", "1","John", "Smith \r\n");
+//    f.format("%s %s %s", "2","Amy", "Brown");
+//    f.close();    
+//  } catch (Exception e) {
+//    System.out.println("Error");
+//  }
+  
+        
+       
+   }
+    
+}
