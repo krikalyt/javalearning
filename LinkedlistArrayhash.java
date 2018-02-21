dae2fffe9e5f46d5a17006f965942503b16ff3b8
krikalyt <34827447+krikalyt@users.noreply.github.com>
2018-02-21 17:22:25 +0530

    Add files via upload

---
+++ LinkedlistArrayhash.java
@@ -0,0 +1,80 @@
+package linkedlistarrayhash;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class LinkedlistArrayhash 
+{
+    public static void main(String[] args) throws Exception
+    {
+//        Random rand = new Random();
+        //Array list
+//        ArrayList<Integer> it = new ArrayList<Integer>(5);
+//        it.add(10);
+//        it.add(15);
+//        it.add(17);
+//        it.add(18);
+//        it.remove(18);
+//        
+//        System.out.println(it.get(0));
+//   int listofnum[] = new int[7];
+//        
+//      LinkedList<Integer> obj = new LinkedList<> ();
+//      
+//         for(int i =0;i<7;i++)
+//      {
+//          listofnum[i] = rand.nextInt(100);
+//      }
+//      for(int i =0;i<7;i++)
+//      {
+//          obj.add(listofnum[i]);
+//      }
+//      
+//      Collections.sort(obj);
+//      System.out.println(obj);
+//      System.out.println(obj.get(0));
+        
+//        HashMap <Integer,String> intstr = new HashMap<>();
+//        intstr.put(1, "Abhay Gupta");
+//        intstr.put(28, "Krishna Yadav");
+//        intstr.put(27, "Khushboo Singh");
+//        
+//        HashMap <String, Integer> strint = new HashMap<>();
+//        strint.put("krishna",28);
+//        System.out.println(strint.get("krishna"));
+        
+//        HashSet<Integer> hasset = new HashSet<>();
+//        hasset.add(67);
+//        hasset.add(45);
+//        hasset.add(72);
+//        System.out.println(hasset);
+//        
+//        System.out.println(hasset.size());
+        
+//        
+//        Collections.sort(obj);
+//        Iterator<Integer> it = obj.iterator();
+//       
+//        while(it.hasNext())
+//        {
+//            System.out.println(it.next());
+//        }
+        
+//       int[] ints = {1, 2, 3};
+//    ArrayList<Integer> intList = new ArrayList<Integer>();
+//    for (int index = 0; index < ints.length; index++)
+//    {
+//        intList.add(ints[index]);
+//    } 
+//    for (int index = 0; index < ints.length; index++)
+//    {
+//        System.out.println(intList.get(index));
+//    } 
+    
+        
+                
+      
+    } 
+}
+ 
